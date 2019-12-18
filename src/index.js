9867ef15aac932210efe7115cde6a28ae7b54c0f
Hashimoto <vinicius.hashimoto@getmore.com.br>
2019-12-18 11:23:44 -0300

    import bootstrap to application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+//Using bootstrap for styling the web application
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/index.css";
+import App from "./screen/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
